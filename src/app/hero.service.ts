1d9019a9f2ad55338066d0d2d311961d5ddd9e0b
Josh Turgeon <jturgeon88@gmail.com>
2018-11-26 13:11:50 -0600

    Adjust to account for asynchronous data fetching

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -12,7 +13,7 @@ export class HeroService {
 
   // this function is simulating a fetch call to a server for heroes. Here we're
   //just returning the mock data of heroes
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
