c629bff7c404be30a621705f7641b3d892c8d0dd
Josh Turgeon <jturgeon88@gmail.com>
2018-11-26 13:51:53 -0600

    Show messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,17 +3,19 @@ import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   // this function is simulating a fetch call to a server for heroes. Here we're
   //just returning the mock data of heroes
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
